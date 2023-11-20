9488def79e3b46d8824d7550ae1cecd37003f2df
Yago Carro <ycarro@c1r1s1.42madrid.com>
2023-11-20 16:00:20 +0100

    all done

---
+++ cpp01/ex05/src/Harl.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycarro <ycarro@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 11:06:29 by ycarro            #+#    #+#             */
+/*   Updated: 2023/11/20 15:55:34 by ycarro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void){}
+Harl::~Harl(void){}
+
+void	Harl::debug(void)
+{
+	std::cout \
+	<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" \
+	<< std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout \
+	<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" \
+	<< std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout \
+	<< "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." \
+	<< std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout \
+	<< "This is unacceptable! I want to speak to the manager now." \
+	<< std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	int i = 0;
+	void	(Harl::*PtF[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string lv[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	while(i < 4)
+	{
+		if (level == lv[i])
+			{
+				(this->*PtF[i])();
+				break;
+			}
+		i++;
+	}	
+}
--- cpp01/ex05/src/main.cpp
+++ cpp01/ex05/src/main.cpp
@@ -6,39 +6,20 @@
 /*   By: ycarro <ycarro@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:06:29 by ycarro            #+#    #+#             */
-/*   Updated: 2023/11/02 12:28:24 by ycarro           ###   ########.fr       */
+/*   Updated: 2023/11/02 14:06:58 by ycarro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-Harl(void){}
-~Harl(void){}
-
-void	debug(void)
+int main(void)
 {
-	std::cout \
-	>> "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" \
-	>> std::endl;
-}
+	Harl karen;
 
-void	info(void)
-{
-	std::cout \
-	>> "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" \
-	>> std::endl;
-}
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
 
-void	warning(void)
-{
-	std::cout \
-	>> "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." \
-	>> std::endl;
-}
-
-void	error(void)
-{
-	std::cout \
-	>> "This is unacceptable! I want to speak to the manager now" \
-	>> std::endl;
+	return(0);
 }
